399cd032ab7a6cff2581edfc372a647cc4473241
Nayela Wickramasuriya <nayelawickramasuriya@Nayelas-MBP-2.fritz.box>
2023-04-02 19:07:12 +0100

    Remove ingredients column from recipes table
    Ingredients column no longer required on recipes table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_01_180032) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_165009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_180032) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ingredients", default: [], array: true
   end
 
   add_foreign_key "recipe_ingredients", "ingredients"
