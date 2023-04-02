07d4a2438f7ff880d7bc61572458977cbb7b69a8
Nayela Wickramasuriya <nayelawickramasuriya@Nayelas-MBP-2.fritz.box>
2023-04-02 18:46:01 +0100

    Add basic test for Ingredient model
    This test ensures that the ingredient search works.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "#search" do
+    let!(:potato) {Ingredient.create(
+      name: "potato"
+    )}
+    let!(:flour) {Ingredient.create(
+      name: "flour"
+    )}
+    let!(:self_raising_flour) {Ingredient.create(
+      name: "self-raising flour"
+    )}
+    it "gets an ingredient by name if it exists" do
+      expect(Ingredient.search("flour").count).to eq(2)
+    end
+  end
 end
