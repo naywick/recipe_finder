018540820f38cd988f54154510b37df4c5baa8d1
Nayela Wickramasuriya <nayelawickramasuriya@Nayelas-MBP-2.fritz.box>
2023-04-02 22:50:41 +0100

    Extend search to handle multiple ingredients
    Now the search will return all recipes that have any of the ingredients passed in as the search string.
    This is the first iteration of improving the search. While this works, what I'd like to do next is ensure that recipes returned each have all of the searched ingredients.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,17 @@
 class IngredientsController < ApplicationController
+  require 'json'
   def index()
     @ingredients = Ingredient.all()
   end
 
   def search()
-    ingredients = Ingredient.search(params[:search])
-    ingredient_ids = ingredients.map { |i| i.id }
-    recipe_ingredients = RecipeIngredient.where(ingredient_id: ingredient_ids)
+    #Get the ingredients ids
+    ingredients_ids = Ingredient.get_ids_by_names(params[:search])
+    puts ingredients_ids.to_json
+
+    #Get the recipe_ingredients where ingredient_id is in the id array
+    recipe_ingredients = RecipeIngredient.where(ingredient_id: ingredients_ids)
+
     recipe_ids = recipe_ingredients.map { |ri| ri.recipe_id }
     @recipes = Recipe.find(recipe_ids)
   end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,14 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
   
   def self.search(search)
-    ingredients = Ingredient.where("name ilike :search", search: "%#{search}%")
+    Ingredient.where("name ilike :search", search: "%#{search}%")
+  end
+
+  def self.get_ids_by_names(names)
+    split_names = names.split(" ")
+    ids = split_names.map do |n|
+      search(n).map { |i| i.id}
+    end
+    ids.flatten
   end
 end
